f99531cc4054817252f62c2a46305a982a68a74e
Tiago França <tiago@tiagofranca.com>
2024-12-04 15:13:23 -0300

    fix f2_str_or_null function
    	modified:   src/global_functions/helpers.php

--- src/global_functions/helpers.php
+++ src/global_functions/helpers.php
@@ -379,7 +379,7 @@ if (!function_exists('bool_or_null')) {
             return null;
         }
 
-        return filter_var($value, FILTER_VALIDATE_BOOL, FILTER_NULL_ON_FAILURE);
+        return filter_var($value, FILTER_DEFAULT, FILTER_NULL_ON_FAILURE);
     }
 }
 
